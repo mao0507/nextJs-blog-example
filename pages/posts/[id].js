074a3effb2daaf7d741730df5d983c5e7aeb0951
mao <mao850507@gmail.com>
2023-11-09 10:18:21 +0800

    feat:調整動態頁面渲染項目

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,7 +4,7 @@ import { getAllPostIds, getPostData } from '../../lib/post';
 
 export async function getStaticPaths() {
   // Return a list of possible value for id
-
+  // 動態抓取檔案路徑，並加入到路由裡面
   const paths = getAllPostIds();
   return {
     paths,
@@ -15,7 +15,8 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   // Fetch necessary data for the blog post using params.id
 
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
+
   return {
     props: {
       postData,
@@ -26,11 +27,11 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <h3>{postData.title}</h3>
+      <h5>{postData.id}</h5>
+      <h5>{postData.date}</h5>
+      <hr />
+      <div dangerouslySetInnerHTML={{ __html: postData.content }} />
     </Layout>
   );
 }
